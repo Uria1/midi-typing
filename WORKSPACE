cb29f46caca5beb76ca0e1b5e50056ef0e9f1341
Uria Ashkenazy Adler <uriaa@wix.com>
2018-08-09 21:16:56 +0300

    first working test. only working in debug mode probably due to the way bazel runs tests

--- WORKSPACE
+++ WORKSPACE
@@ -1,1 +1,27 @@
 
+workspace(name = "midityping")
+
+rules_scala_version="74a91254f7e2335496813407e210ac00542cd44e" # update this as needed
+
+rules_scala_version_sha256="ed022fbee43ab9bcafab76b4054bcc92267519f78d7d9e0521be95761e279aaa"
+
+http_archive(
+             name = "io_bazel_rules_scala",
+             url = "https://github.com/wix/rules_scala/archive/%s.zip"%rules_scala_version,
+             type = "zip",
+             strip_prefix= "rules_scala-%s" % rules_scala_version,
+             sha256 = rules_scala_version_sha256,
+)
+
+load("@io_bazel_rules_scala//scala:scala.bzl", "scala_repositories")
+scala_repositories()
+
+# register default scala toolchain
+load("@io_bazel_rules_scala//scala:toolchains.bzl", "scala_register_toolchains")
+scala_register_toolchains()
+
+#load("//3rdparty:workspace.bzl", "maven_dependencies")
+#maven_dependencies()
+
+load("@io_bazel_rules_scala//specs2:specs2_junit.bzl","specs2_junit_repositories")
+specs2_junit_repositories()
---
+++ poc/src/test/scala/midityping/PocTest.scala
@@ -0,0 +1,50 @@
+package poc.src.test.scala.midityping
+
+import javax.sound.midi._
+import org.specs2.mutable.SpecificationWithJUnit
+
+import scala.util.Try
+
+class PocTest extends SpecificationWithJUnit {
+  "Midi" should {
+
+    "work" in {
+      val devices = MidiSystem.getMidiDeviceInfo()
+      devices.foreach(deviceInfo => {
+        Try {
+          println(s"###### $deviceInfo #######")
+          Thread.sleep(1000)
+          val device = MidiSystem.getMidiDevice(deviceInfo)
+          device.getTransmitter.setReceiver(new R)
+          device.open()
+          Thread.sleep(2000)
+          device.close()
+        }.recover {
+          case ex: MidiUnavailableException =>
+          case ex => ex.printStackTrace()
+        }
+      })
+      ko
+    }
+  }
+
+  class R extends Receiver {
+    override def send(message: MidiMessage, timeStamp: Long): Unit = {
+      message match {
+        case sm: ShortMessage =>
+          println(s"channel: ${sm.getChannel}")
+          println(s"command: ${sm.getCommand}")
+          println(s"D1: ${sm.getData1}")
+          println(s"D2: ${sm.getData2}")
+        case _ => println(message)
+      }
+      println(message.getMessage)
+    }
+
+    override def close(): Unit = {
+
+    }
+  }
+
+}
+
