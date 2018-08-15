0a9a54013e6834bf269b2694b8ea18b8d2d26ae5
Uria Ashkenazy Adler <uriaa@wix.com>
2018-08-15 09:05:02 +0300

    change meaning of note name and add full name

--- poc/src/main/scala/org/midityping/poc/common/Note.scala
+++ poc/src/main/scala/org/midityping/poc/common/Note.scala
@@ -1,6 +1,8 @@
 package org.midityping.poc.common
 
-case class Note(name: String, octave: Int, number: Int)
+case class Note(name: String, octave: Int, number: Int) {
+  def fullName: String = name + octave
+}
 
 object Note {
   private val notes = "C C#D D#E F F#G G#A A#B "
@@ -9,7 +11,7 @@ object Note {
     val octave = noteNumber / 12 - 1
     val noteIndex: Int = (noteNumber % 12) * 2
     val noteName = notes.substring(noteIndex, noteIndex + 2).trim
-    Note(noteName + octave, octave, noteNumber)
+    Note(noteName, octave, noteNumber)
   }
 
   def C4 = Note(60)
