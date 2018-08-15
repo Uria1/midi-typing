1f883b951bb7e627b17e123b442f78e7848b88a6
Uria Ashkenazy Adler <uriaa@wix.com>
2018-08-15 07:51:03 +0300

    improve note class

--- poc/src/main/scala/org/midityping/poc/common/Note.scala
+++ poc/src/main/scala/org/midityping/poc/common/Note.scala
@@ -5,10 +5,11 @@ case class Note(name: String, octave: Int, number: Int)
 object Note {
   private val notes = "C C#D D#E F F#G G#A A#B "
 
-  def apply(noteNumber:Int): Note = {
+  def apply(noteNumber: Int): Note = {
     val octave = noteNumber / 12 - 1
-    val noteName = notes.substring((noteNumber % 12) * 2, (noteNumber % 12) * 2 + 2).trim
-    new Note(noteName + octave, octave, noteNumber)
+    val noteIndex: Int = (noteNumber % 12) * 2
+    val noteName = notes.substring(noteIndex, noteIndex + 2).trim
+    Note(noteName + octave, octave, noteNumber)
   }
 
   def C4 = Note(60)
