670241ed884b564317b9190683e7941f9d3e11b0
Uria Ashkenazy Adler <uriaa@wix.com>
2018-08-15 06:12:16 +0300

    cleanup

--- poc/src/main/scala/org/midityping/poc/JsonMappingLoader.scala
+++ poc/src/main/scala/org/midityping/poc/JsonMappingLoader.scala
@@ -7,26 +7,15 @@ import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 
 import scala.collection.JavaConverters._
-//import collection.JavaConversions._
-import collection.mutable._
 
 class JsonMappingLoader {
   def load(resourceFilename: String): Mapping = {
     val gson = new GsonBuilder().create
 
-    val m = Mapping(Map(
-      EventDescriptor(EventType.MidiNoteOn, "85").asKey -> ActionDescriptor(ActionType.KeyPress, "a")
-    ))
-
     val mapType = new TypeToken[util.Map[String, ActionDescriptor]] {}.getType
-
-    gson.toJson(m.map.asJava, mapType)
 
     val resource = getClass.getResourceAsStream(resourceFilename)
     val map = gson.fromJson[util.Map[String, ActionDescriptor]](new BufferedReader(new InputStreamReader(resource)), mapType)
     Mapping(map.asScala)
   }
 }
-
-//{"key1":{"eventType":"MidiNoteOn","arg":"85"},"value1":{"actionType":"KeyPress","arg":"a"}}
-//{"key1":{"eventType":"MidiNoteOn","arg":"85"},"value1":{"actionType":"KeyPress","arg":"a"},"key2":{"eventType":"MidiNoteOn","arg":"86"},"value2":{"actionType":"KeyPress","arg":"b"}}
