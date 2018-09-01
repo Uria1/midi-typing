1c26e74e6269a74b0f02bf2dcd5cd01357c0ef9b
Uria Ashkenazy Adler <uriaa@wix.com>
2018-09-01 23:53:01 +0300

    move logger val

--- poc/src/main/scala/org/midityping/poc/actions/DefaultActionFactory.scala
+++ poc/src/main/scala/org/midityping/poc/actions/DefaultActionFactory.scala
@@ -3,8 +3,9 @@ package org.midityping.poc.actions
 import org.midityping.poc.logging.aLogger
 
 class DefaultActionFactory extends ActionFactory {
+  val logger = aLogger.forClass(getClass)
+
   override def createAction(descriptor: ActionDescriptor): Action = {
-    val logger = aLogger.forClass(getClass)
     logger.trace(s"creating action for $descriptor")
     ActionType.withName(descriptor.actionType) match {
       case ActionType.KeyPress =>
